0a7401c35b5278ce29ea2ffaa70449e7e135e169
Shady Idrissi <chadiidac@gmail.com>
2024-06-28 15:30:48 +0100

    conect from with eamilJS

--- service/src/app/function/Form.js
+++ service/src/app/function/Form.js
@@ -1,10 +1,46 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import React from "react";
+import emailjs from "@emailjs/browser";
 
 function Form(props) {
+  const form = useRef();
   const [countries, setCountries] = useState([]);
   const [selectedCountry, setSelectedCountry] = useState("");
+
+  const getDeploymentOptions = () => {
+    const checkboxes = form.current.querySelectorAll('input[name^="comments-"]:checked');
+    return Array.from(checkboxes).map(checkbox => checkbox.value).join(', ');
+  };
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    const deploymentOptions = getDeploymentOptions();
+
+    const formData = new FormData(form.current);
+    formData.append('option', deploymentOptions);
+
+    emailjs
+      .send("service_kl7jslm", "template_3mfyzla", {
+        firstName: formData.get("firstName"),
+        lastName: formData.get("lastName"),
+        country: formData.get("country"),
+        email: formData.get("email"),
+        functionType: formData.get("functionType"),
+        title:props.title,
+        budge: formData.get("budge"),
+        option: deploymentOptions,
+      }, "OUFjExamyWKiw3ZGc")
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
 
   useEffect(() => {
     fetch(
@@ -17,13 +53,9 @@ function Form(props) {
       });
   }, []);
 
-  useEffect(() => {
-    console.log(countries);
-  }, [countries]);
-
   return (
     <>
-      <form>
+      <form ref={form} onSubmit={sendEmail}>
         <div className="space-y-6" style={{ width: "70%", margin: "20px 15%" }}>
           <div className="border-b border-gray-900/10 pb-12">
             <h2 className="text-base font-semibold leading-7 text-gray-900">
@@ -44,7 +76,7 @@ function Form(props) {
                 <div className="mt-2">
                   <input
                     type="text"
-                    name="first-name"
+                    name="firstName"
                     id="first-name"
                     autoComplete="given-name"
                     className="block w-full py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-indigo-300 sm:text-sm sm:leading-6"
@@ -62,7 +94,7 @@ function Form(props) {
                 <div className="mt-2">
                   <input
                     type="text"
-                    name="last-name"
+                    name="lastName"
                     id="last-name"
                     autoComplete="family-name"
                     className="block w-full py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-indigo-300 sm:text-sm sm:leading-6"
@@ -100,6 +132,7 @@ function Form(props) {
                     className="block w-full border-1 px-2 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
                     value={selectedCountry}
                     onChange={(e) => setSelectedCountry(e.target.value)}
+                    name="country"
                   >
                     {countries.length > 0 ? (
                       countries.map((country) => (
@@ -116,16 +149,16 @@ function Form(props) {
 
               <div className="sm:col-span-3">
                 <label
-                  htmlFor="street-address"
+                  htmlFor="function-type"
                   className="block text-medium font-medium leading-6 text-gray-900"
                 >
-                  {props.f}
+                  Function Type
                 </label>
                 <div className="mt-2">
                   <input
                     type="text"
-                    name="street-address"
-                    id="street-address"
+                    name="functionType"
+                    id="function-type"
                     autoComplete="street-address"
                     className="block w-full py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-indigo-300 sm:text-sm sm:leading-6"
                   />
@@ -151,6 +184,7 @@ function Form(props) {
                           id={`comments-${index}`}
                           name={`comments-${index}`}
                           type="checkbox"
+                          value={e}
                           className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
                         />
                       </div>
@@ -175,8 +209,9 @@ function Form(props) {
                     <div className="flex items-center gap-x-3" key={index}>
                       <input
                         id={`price-${index}`}
-                        name="budget"
+                        name="budge"
                         type="radio"
+                        value={e}
                         className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
                       />
                       <label
