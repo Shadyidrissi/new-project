49791f4d4f3ec9e4e9ff3283c6831e0f96e85757
Shady Idrissi <chadiidac@gmail.com>
2024-06-30 23:22:21 +0100

    add analiys on my web

--- service/src/app/layout.tsx
+++ service/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from '../../components/Footer'
 import Navbar  from '../../components/Navbar'
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
